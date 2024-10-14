fd373f7c7342d8037d7527d4ca8676e1082c4551
Nikitastolyarow <nikita.stolyarow2002@yandex.ru>
2024-10-14 23:56:10 +0300

    Добавление коллекции

--- src/main/java/homework/Request.java
+++ src/main/java/homework/Request.java
@@ -1,13 +1,19 @@
 package homework;
 
 import java.io.InputStream;
+import java.util.List;
 import java.util.Map;
+
+
+import java.util.stream.Collectors;
+import org.apache.http.NameValuePair;
 
 public class Request {
     private final String method;
     private final String path;
     private final Map<String, String> headers;
     private final InputStream body;
+    private List<NameValuePair> queryParams;
 
     public Request(String method, String path, Map<String, String> headers, InputStream body) {
         this.method = method;
@@ -24,13 +30,28 @@ public class Request {
         return path;
     }
 
+
+    public void setQueryParams(List<NameValuePair> params) {
+        this.queryParams = params;
+    }
+    public List<NameValuePair> getQueryParams() {
+        return queryParams;
+    }
+    
+    public String getQueryParam(String param) {
+        return queryParams.stream()
+                .filter(p -> p.getName().equals(param))
+                .map(p -> p.getValue())
+                .collect(Collectors.joining(", "));
+    }
     @Override
     public String toString() {
         return "Request{" +
-                "method='" + method + '\'' +
-                ", path='" + path + '\'' +
-                ", headers=" + headers +
-                ", body='" + body + '\'' +
+                "\n method='" + method + '\'' +
+                "\n path='" + path + '\'' +
+                "\n headers=" + headers +
+                "\n body='" + body + '\'' +
+                "\n queryParams=" + queryParams +
                 '}';
     }
 }
